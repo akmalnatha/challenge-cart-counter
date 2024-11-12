a599108544ed1acbf950f38631883a875bbc4fc0
Akmal Natha Abhirama <akmalabhirama2002@gmail.com>
2024-11-12 20:35:59 +0700

    refactor: add export to interfaces

--- src/lib/types/Cart.ts
+++ src/lib/types/Cart.ts
@@ -1,4 +1,6 @@
-interface Cart {
+import { CartItem } from "./CartItem";
+
+export interface CartInterface {
   userId: number;
   date?: string;
   products: CartItem[];
--- src/lib/types/CartItem.ts
+++ src/lib/types/CartItem.ts
@@ -1,4 +1,6 @@
-interface CartItem extends Omit<Product, "id"> {
+import { Product } from "./Product";
+
+export interface CartItem extends Omit<Product, "id"> {
   productId: number;
   quantity: number;
 }
